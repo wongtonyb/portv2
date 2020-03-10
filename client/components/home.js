4594476366597fbd41c6d17313c77c9c83799f4a
tony <wongtonyb@gmail.com>
2020-03-10 00:29:11 -0400

    home

--- client/components/home.js
+++ client/components/home.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
     return (
-      <div className="comp" id="home">
-        <div id="about-text-container">
+      <div className="comp has-overlay" id="home">
+        <div id="home-text-container">
           <h2>Hi I'm Tony</h2>
           <h1>A FULLSTACK SOFTWARE ENGINEER</h1>
           <h3>
@@ -14,7 +14,10 @@ class Home extends Component {
           <h3>
             I enjoy building dynamic fullstack web and mobile applications using
             popular technologies such as NodeJS, React, Redux, PostgreSQL, and
-            many <span>more</span>
+            many{" "}
+            <span>
+              <a href="#technologies">more</a>
+            </span>
           </h3>
         </div>
         <img src="./assets/etc/op-me.png" />
